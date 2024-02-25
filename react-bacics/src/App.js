5614021dd40245b43267ef6e6bde6795258594f9
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2024-02-25 00:45:31 +0000

    Added scoring system and a lot of questions

--- react-bacics/src/App.js
+++ react-bacics/src/App.js
@@ -5,9 +5,9 @@ function App() {
 
 const[question,setQuestion] = useState("")
 const[lastClicked,setLastClicked] = useState(0)
-const[scores,setScores] = useState([{"name":"messi","score":300}])
+const[scores,setScores] = useState([{"name":"Messi","score":300}])
 
-console.log(lastClicked)
+
   return (
     <div className="App">
       <div className="board">
@@ -16,30 +16,30 @@ console.log(lastClicked)
         <Catz txt="Name That Instrument!"></Catz>
         <Catz txt="-Ology"></Catz>
         <Catz txt="Slang Terms"></Catz>
-        <Catz txt="State Abreviations"></Catz>
-        <Card number={100} question = "Afghani" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={100} question = "Jake Paul" setQuestion = {setQuestion}setLastClicked={setLastClicked}></Card>
-        <Card number={100} question="Jason Derulo made a song named after this instrument" setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
-        <Card number={100} question = "The study of human history and prehistory through excavation"setLastClicked={setLastClicked} setQuestion={setQuestion}></Card>
-        <Card number={100} question = " ''Cap'' " setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
-        <Card number={100} question = "''IL.''" setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
-        <Card number={200} question = "Real" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={200} question = "Scott Swift" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={200} question = "High pitched, woody, sort of sound." setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={200} question = "The study of humans" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={200} question = "''Rizz''" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
-        <Card number={200}setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={300} setLastClicked={setLastClicked}></Card>
-        <Card number={400} setLastClicked={setLastClicked}></Card>
-        <Card number={400} setLastClicked={setLastClicked}></Card>
-        <Card number={400} setLastClicked={setLastClicked}></Card>
-        <Card number={400} setLastClicked={setLastClicked}></Card>
-        <Card number={400} setLastClicked={setLastClicked}></Card>
+        <Catz txt="State Capitals"></Catz>
+        <Card number={100} question={question} cardquestion = "Afghani" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={100} question={question} cardquestion = "Jake Paul" setQuestion = {setQuestion}setLastClicked={setLastClicked}></Card>
+        <Card number={100} question={question} cardquestion="Loud metalic sound created by crashing them together" setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
+        <Card number={100} question={question} cardquestion = "The study of human history and prehistory through excavation"setLastClicked={setLastClicked} setQuestion={setQuestion}></Card>
+        <Card number={100} question={question} cardquestion = " ''Cap'' " setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
+        <Card number={100} question={question} cardquestion = "Albany" setQuestion={setQuestion}setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "Rupee" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "Scott Swift" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "High pitched, woody, sort of sound." setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "The study of humans" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "''Rizz''" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={200} question={question} cardquestion = "Sacrimento" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "Shekels" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "Denise Jonas" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "Jason Derulo made a song named after this instrument" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "The study of medicine involving skin" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "''Opps''" setQuestion={setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={300} question={question} cardquestion = "Baton Rouge" setQuestion={setQuestion}  setLastClicked={setLastClicked}></Card>
+        <Card number={400} question={question} cardquestion = "Lire" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={400} question={question} cardquestion = "Nancy Dow" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={400} question={question} cardquestion = "Large string instrument played with fingers" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={400} question={question} cardquestion = "The study of eggs" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
+        <Card number={400} question={question} cardquestion = "Gatekeeping(Gatekeeper)" setQuestion = {setQuestion} setLastClicked={setLastClicked}></Card>
         <Card number={400} setLastClicked={setLastClicked}></Card>
         <Card number={500} setLastClicked={setLastClicked} ></Card>
         <Card number={500} setLastClicked={setLastClicked}></Card>
@@ -53,7 +53,7 @@ console.log(lastClicked)
     </div>
   );
 }
-function Score( lastClicked, teamName,score){
+function Score({lastClicked, teamName,score}){
 return(
   <div className="father">
     <div className="soccerMom">
@@ -75,14 +75,14 @@ function Module({question, setQuestion}) {
     </div>
   )
 }
-function Card({number, question, setQuestion, setLastClicked}) {
+function Card({number, question, cardquestion, setQuestion, setLastClicked}) {
   const clickHandler = ()=>{
-    setQuestion(question)
+    setQuestion(cardquestion)
     setLastClicked(number)
-    
+    //.style.backgroundColor = "#fffff"
   }
   return(
-    <div className="card" onClick={clickHandler}>
+    <div className={question == cardquestion? "clicked": "card"}onClick={clickHandler}>
       <p>${number}</p>
      
     </div>
